29cc63815e228a4c55ce92f2e7e65e9a5dbdb9ec
David <daviddvega@gmail.com>
2020-03-07 23:20:37 -0600

    first

---
+++ MainWindow.py
@@ -0,0 +1,245 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'mainwindow.ui'
+#
+# Created by: PyQt5 UI code generator 5.14.1
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(342, 342)
+        MainWindow.setStyleSheet("background-color: rgb(0, 0, 0);")
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.gridLayout_2 = QtWidgets.QGridLayout(self.centralwidget)
+        self.gridLayout_2.setObjectName("gridLayout_2")
+        self.mainScreen = QtWidgets.QListWidget(self.centralwidget)
+        self.mainScreen.setMouseTracking(True)
+        self.mainScreen.setFocusPolicy(QtCore.Qt.TabFocus)
+        self.mainScreen.setStyleSheet("color: rgb(180, 152, 124);\n"
+"background-color: rgb(51, 51, 29);\n"
+"font: 75 11pt \"Menlo\";\n"
+"selection-color: rgb(22, 22, 22);\n"
+"selection-background-color: rgb(139, 56, 24);\n"
+"\n"
+"\n"
+"")
+        self.mainScreen.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.mainScreen.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.mainScreen.setLineWidth(1)
+        self.mainScreen.setMidLineWidth(0)
+        self.mainScreen.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOn)
+        self.mainScreen.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        self.mainScreen.setSizeAdjustPolicy(QtWidgets.QAbstractScrollArea.AdjustIgnored)
+        self.mainScreen.setAutoScroll(False)
+        self.mainScreen.setTextElideMode(QtCore.Qt.ElideMiddle)
+        self.mainScreen.setResizeMode(QtWidgets.QListView.Adjust)
+        self.mainScreen.setViewMode(QtWidgets.QListView.ListMode)
+        self.mainScreen.setSelectionRectVisible(False)
+        self.mainScreen.setObjectName("mainScreen")
+        self.gridLayout_2.addWidget(self.mainScreen, 0, 0, 1, 1)
+        self.stackedWidget = QtWidgets.QStackedWidget(self.centralwidget)
+        self.stackedWidget.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.stackedWidget.setObjectName("stackedWidget")
+        self.basic = QtWidgets.QWidget()
+        self.basic.setObjectName("basic")
+        self.basEnterButt = QtWidgets.QPushButton(self.basic)
+        self.basEnterButt.setGeometry(QtCore.QRect(320, 0, 101, 31))
+        self.basEnterButt.setStyleSheet("background-color: rgb(0, 42, 12);\n"
+"color: rgb(179, 147, 112);\n"
+"font: 75 15pt \"Menlo\";")
+        self.basEnterButt.setObjectName("basEnterButt")
+        self.stackedWidget.addWidget(self.basic)
+        self.scientific = QtWidgets.QWidget()
+        self.scientific.setObjectName("scientific")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.scientific)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.factorialButt = QtWidgets.QPushButton(self.scientific)
+        self.factorialButt.setStyleSheet("background-color: rgb(0, 42, 12);\n"
+"color: rgb(179, 147, 112);\n"
+"font: 75 12pt \"Menlo\";")
+        self.factorialButt.setObjectName("factorialButt")
+        self.verticalLayout.addWidget(self.factorialButt)
+        self.pushButton = QtWidgets.QPushButton(self.scientific)
+        self.pushButton.setStyleSheet("background-color: rgb(0, 42, 12);\n"
+"color: rgb(179, 147, 112);\n"
+"font: 75 12pt \"Menlo\";")
+        self.pushButton.setObjectName("pushButton")
+        self.verticalLayout.addWidget(self.pushButton)
+        self.pushButton_3 = QtWidgets.QPushButton(self.scientific)
+        self.pushButton_3.setStyleSheet("background-color: rgb(0, 42, 12);\n"
+"color: rgb(179, 147, 112);\n"
+"font: 75 12pt \"Menlo\";")
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.verticalLayout.addWidget(self.pushButton_3)
+        self.sqRtButt = QtWidgets.QPushButton(self.scientific)
+        self.sqRtButt.setStyleSheet("background-color: rgb(0, 42, 12);\n"
+"color: rgb(179, 147, 112);\n"
+"font: 75 12pt \"Menlo\";")
+        self.sqRtButt.setObjectName("sqRtButt")
+        self.verticalLayout.addWidget(self.sqRtButt)
+        self.log10Butt = QtWidgets.QPushButton(self.scientific)
+        self.log10Butt.setStyleSheet("background-color: rgb(0, 42, 12);\n"
+"color: rgb(179, 147, 112);\n"
+"font: 75 12pt \"Menlo\";")
+        self.log10Butt.setObjectName("log10Butt")
+        self.verticalLayout.addWidget(self.log10Butt)
+        self.log2Butt = QtWidgets.QPushButton(self.scientific)
+        self.log2Butt.setStyleSheet("background-color: rgb(0, 42, 12);\n"
+"color: rgb(179, 147, 112);\n"
+"font: 75 12pt \"Menlo\";")
+        self.log2Butt.setObjectName("log2Butt")
+        self.verticalLayout.addWidget(self.log2Butt)
+        self.pushButton_6 = QtWidgets.QPushButton(self.scientific)
+        self.pushButton_6.setStyleSheet("background-color: rgb(0, 42, 12);\n"
+"color: rgb(179, 147, 112);\n"
+"font: 75 12pt \"Menlo\";")
+        self.pushButton_6.setObjectName("pushButton_6")
+        self.verticalLayout.addWidget(self.pushButton_6)
+        self.pushButton_2 = QtWidgets.QPushButton(self.scientific)
+        self.pushButton_2.setStyleSheet("background-color: rgb(0, 42, 12);\n"
+"color: rgb(179, 147, 112);\n"
+"font: 75 12pt \"Menlo\";")
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.verticalLayout.addWidget(self.pushButton_2)
+        self.stackedWidget.addWidget(self.scientific)
+        self.ham = QtWidgets.QWidget()
+        self.ham.setObjectName("ham")
+        self.verticalLayout_4 = QtWidgets.QVBoxLayout(self.ham)
+        self.verticalLayout_4.setObjectName("verticalLayout_4")
+        self.cboxbits = QtWidgets.QComboBox(self.ham)
+        self.cboxbits.setStyleSheet("background-color: rgb(0, 42, 12);\n"
+"color: rgb(179, 147, 112);\n"
+"font: 75 12pt \"Menlo\";")
+        self.cboxbits.setObjectName("cboxbits")
+        self.cboxbits.addItem("")
+        self.cboxbits.addItem("")
+        self.cboxbits.addItem("")
+        self.verticalLayout_4.addWidget(self.cboxbits)
+        self.cBoxoddeven = QtWidgets.QComboBox(self.ham)
+        self.cBoxoddeven.setStyleSheet("background-color: rgb(0, 42, 12);\n"
+"color: rgb(179, 147, 112);\n"
+"font: 75 12pt \"Menlo\";")
+        self.cBoxoddeven.setObjectName("cBoxoddeven")
+        self.cBoxoddeven.addItem("")
+        self.cBoxoddeven.addItem("")
+        self.verticalLayout_4.addWidget(self.cBoxoddeven)
+        self.hamEncode = QtWidgets.QPushButton(self.ham)
+        self.hamEncode.setStyleSheet("background-color: rgb(0, 42, 12);\n"
+"color: rgb(179, 147, 112);\n"
+"font: 75 12pt \"Menlo\";")
+        self.hamEncode.setObjectName("hamEncode")
+        self.verticalLayout_4.addWidget(self.hamEncode)
+        self.stackedWidget.addWidget(self.ham)
+        self.binaryCon = QtWidgets.QWidget()
+        self.binaryCon.setObjectName("binaryCon")
+        self.stackedWidget.addWidget(self.binaryCon)
+        self.gridLayout_2.addWidget(self.stackedWidget, 0, 1, 1, 1)
+        self.menu = QtWidgets.QComboBox(self.centralwidget)
+        font = QtGui.QFont()
+        font.setFamily("Menlo")
+        font.setPointSize(15)
+        font.setBold(False)
+        font.setItalic(False)
+        font.setWeight(9)
+        self.menu.setFont(font)
+        self.menu.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.menu.setStyleSheet("color: rgb(179, 147, 112);\n"
+"font: 75 15pt \"Menlo\";\n"
+"background-color: rgb(8, 8, 8);\n"
+"")
+        self.menu.setObjectName("menu")
+        self.menu.addItem("")
+        self.menu.addItem("")
+        self.menu.addItem("")
+        self.menu.addItem("")
+        self.menu.addItem("")
+        self.menu.addItem("")
+        self.gridLayout_2.addWidget(self.menu, 1, 0, 1, 2)
+        self.entry = QtWidgets.QLineEdit(self.centralwidget)
+        self.entry.setStyleSheet("color: rgb(0, 0, 0);\n"
+"background-color: rgb(180, 152, 124);\n"
+"font: 75 18pt \"Menlo\";\n"
+"")
+        self.entry.setObjectName("entry")
+        self.gridLayout_2.addWidget(self.entry, 2, 0, 1, 2)
+        self.widget = QtWidgets.QWidget(self.centralwidget)
+        self.widget.setObjectName("widget")
+        self.gridLayout = QtWidgets.QGridLayout(self.widget)
+        self.gridLayout.setObjectName("gridLayout")
+        self.readMeButt = QtWidgets.QPushButton(self.widget)
+        self.readMeButt.setStyleSheet("background-color: rgb(0, 42, 12);\n"
+"color: rgb(179, 147, 112);\n"
+"font: 75 12pt \"Menlo\";")
+        self.readMeButt.setObjectName("readMeButt")
+        self.gridLayout.addWidget(self.readMeButt, 0, 0, 1, 1)
+        self.clearButt = QtWidgets.QPushButton(self.widget)
+        self.clearButt.setStyleSheet("background-color: rgb(0, 42, 12);\n"
+"color: rgb(179, 147, 112);\n"
+"font: 75 12pt \"Menlo\";")
+        self.clearButt.setObjectName("clearButt")
+        self.gridLayout.addWidget(self.clearButt, 0, 1, 1, 1)
+        self.pushButton_4 = QtWidgets.QPushButton(self.widget)
+        self.pushButton_4.setStyleSheet("background-color: rgb(0, 42, 12);\n"
+"color: rgb(179, 147, 112);\n"
+"font: 75 12pt \"Menlo\";")
+        self.pushButton_4.setObjectName("pushButton_4")
+        self.gridLayout.addWidget(self.pushButton_4, 0, 2, 1, 1)
+        self.pushButton_5 = QtWidgets.QPushButton(self.widget)
+        self.pushButton_5.setStyleSheet("background-color: rgb(0, 42, 12);\n"
+"color: rgb(179, 147, 112);\n"
+"font: 75 12pt \"Menlo\";")
+        self.pushButton_5.setObjectName("pushButton_5")
+        self.gridLayout.addWidget(self.pushButton_5, 0, 3, 1, 1)
+        self.gridLayout_2.addWidget(self.widget, 3, 0, 1, 2)
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        self.stackedWidget.setCurrentIndex(1)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.basEnterButt.setText(_translate("MainWindow", "ENTER"))
+        self.factorialButt.setText(_translate("MainWindow", "X!"))
+        self.pushButton.setText(_translate("MainWindow", "test"))
+        self.pushButton_3.setText(_translate("MainWindow", "test"))
+        self.sqRtButt.setText(_translate("MainWindow", "sRoot"))
+        self.log10Butt.setText(_translate("MainWindow", "LOG10"))
+        self.log2Butt.setText(_translate("MainWindow", "LOG2"))
+        self.pushButton_6.setText(_translate("MainWindow", "test"))
+        self.pushButton_2.setText(_translate("MainWindow", "test"))
+        self.cboxbits.setItemText(0, _translate("MainWindow", "8bit"))
+        self.cboxbits.setItemText(1, _translate("MainWindow", "16bit"))
+        self.cboxbits.setItemText(2, _translate("MainWindow", "32bit"))
+        self.cBoxoddeven.setItemText(0, _translate("MainWindow", "odd"))
+        self.cBoxoddeven.setItemText(1, _translate("MainWindow", "even"))
+        self.hamEncode.setText(_translate("MainWindow", "ENCODE"))
+        self.menu.setItemText(0, _translate("MainWindow", "Basic Mode"))
+        self.menu.setItemText(1, _translate("MainWindow", "Scientific Mode"))
+        self.menu.setItemText(2, _translate("MainWindow", "Hamming Code Generator"))
+        self.menu.setItemText(3, _translate("MainWindow", "Hamming Code Reader"))
+        self.menu.setItemText(4, _translate("MainWindow", "Dec2Binary"))
+        self.menu.setItemText(5, _translate("MainWindow", "Binary2Dec"))
+        self.entry.setToolTip(_translate("MainWindow", "<html><head/><body><p>enter equation then hit RETURN</p></body></html>"))
+        self.readMeButt.setText(_translate("MainWindow", "info"))
+        self.clearButt.setText(_translate("MainWindow", "CLS"))
+        self.pushButton_4.setText(_translate("MainWindow", "test"))
+        self.pushButton_5.setText(_translate("MainWindow", "test"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
---
+++ calculator.py
@@ -0,0 +1,112 @@
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+import pyperclip
+import operator
+import texteditor
+
+from MainWindow import Ui_MainWindow
+
+# Calculator state.
+READY = 0
+INPUT = 1
+
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        self.setupUi(self)
+        self.basEnterButt.pressed.connect(self.list_add_basic)
+        self.clearButt.pressed.connect(self.clearEntry)
+        # self.hamEncode.pressed.connect(self.)
+        # self.loadButt.pressed.connect(self.useLog)
+        self.readMeButt.pressed.connect(self.read_me_open)
+        self.stackedWidget.setCurrentIndex(0)
+        self.menu.activated[str].connect(self.menu_changed)
+        self.mainScreen.keyPressEvent = self.keyPressEvent
+        self.mainScreen.itemSelectionChanged.connect(self.on_change)
+        # self.expScreen.itemSelectionChanged.connect(self.expScrChanged)
+
+        # holds value selected in mainScreen
+        self.itemSelected = ""
+        self.equation = ""
+
+        self.show()
+
+    def clearEntry(self):
+        self.entry.clear()
+
+    def on_change(self):
+        for item in self.mainScreen.selectedItems():
+            self.itemSelected = item.text()
+            self.entry.setText(self.itemSelected)
+            print(self.itemSelected)
+
+    # def expScrChanged(self):
+    #     for item in self.expScreen.selectedItems():
+    #         self.itemSelected = item.text()
+    #         self.entry.setText(self.itemSelected)
+    #         print(self.itemSelected)
+
+    # what happens when you push enter key
+    def keyPressEvent(self, e):
+        print("event", e)
+        if e.key() == Qt.Key_Return:
+            if self.menu.currentText() == 'Basic Mode':
+                self.list_add_basic()
+            elif self.menu.currentText() == 'Hamming Code Generator':
+                self.list_add_ham()
+            else:
+                return
+
+    def useLog(self):
+        print("hello there")
+
+    # def display(self):
+    #     self.outputScreen.insertPlainText(self.equation)
+
+    def onClick(self):
+        self.evalulate()
+
+    def list_add_basic(self):
+        tval = self.evalulate()
+        self.entry.setText("")
+        self.mainScreen.addItem(tval)
+        # self.expScreen.addItem(self.expression)
+        self.expression = ""
+
+    def list_add_ham(self):
+        self.mainScreen.addItem(self.entry.displayText())
+
+    def evalulate(self):
+        self.expression = self.entry.displayText()
+        # self.outputScreen.clear()
+        self.equation = str(eval(self.entry.displayText()))
+        # self.display()
+        return str(eval(self.entry.displayText()))
+
+    def read_me_open(self):
+        texteditor.open(filename="readme.txt")
+
+    def menu_changed(self):
+        value = self.menu.currentText()
+
+        if value == 'Basic Mode':
+            self.stackedWidget.setCurrentIndex(0)
+
+        if value == 'Scientific Mode':
+            self.stackedWidget.setCurrentIndex(1)
+
+        if value == 'Hamming Code Generator':
+            self.stackedWidget.setCurrentIndex(2)
+
+        if value == 'Dec2Binary':
+            self.stackedWidget.setCurrentIndex(3)
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+    app.setApplicationName("TinyCalc")
+
+    window = MainWindow()
+    app.exec_()
